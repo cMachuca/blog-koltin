9f86badfb6f2dfc649901ea5559af95a8761d569
Cristian <cristian.machuca.flores@gmail.com>
2023-11-14 00:51:40 -0800

    cleanup and adding try and catch

--- app/Http/Controllers/Api/PostController.php
+++ app/Http/Controllers/Api/PostController.php
@@ -5,44 +5,47 @@ namespace App\Http\Controllers\Api;
 use App\Http\Controllers\Controller;
 use App\Http\Requests\StorePostRequest;
 use App\Models\Post;
+use Illuminate\Database\UniqueConstraintViolationException;
 use Illuminate\Http\JsonResponse;
-use Illuminate\Http\Request;
+use Illuminate\Log\Logger;
 use Illuminate\Support\Facades\Response;
 use Illuminate\Support\Facades\Auth;
 use Illuminate\Support\Str;
 
 class PostController extends Controller
 {
-    /**
-     * Display a listing of the resource.
-     */
-    public function index(): \Illuminate\Http\JsonResponse
+    public function __construct(public Logger $logger)
+    {
+    }
+
+    public function index(): JsonResponse
     {
         $posts = Post::all()->load(['author', 'comments'])->take(10);
 
         return Response::json($posts);
     }
 
-
     public function store(StorePostRequest $request): JsonResponse
     {
-        $post = new Post();
+        try {
+            $post = new Post();
 
-        $post->title = $request->get('title');
-        $post->slug = Str::slug($request->get('title'));
-        $post->user_id = Auth::user()->id;
-        $post->content = strip_tags($request->get('content'));
+            $post->title = $request->get('title');
+            $post->slug = Str::slug($request->get('title'));
+            $post->user_id = Auth::user()->id;
+            $post->content = strip_tags($request->get('content'));
 
-        $post->save();
+            $post->save();
 
-        return Response::json($post);
-    }
+            return Response::json($post);
+        } catch (UniqueConstraintViolationException $exception) {
+            $this->logger->error($exception->getMessage(), $exception->getTrace());
 
-    /**
-     * Display the specified resource.
-     */
-    public function show(string $id)
-    {
-        //
+            return Response::json(['errors' => ['message' => 'Title already exist.']], 400);
+        } catch (\Exception $exception) {
+            $this->logger->error($exception->getMessage(), $exception->getTrace());
+
+            return Response::json(['errors' => ['message' => 'We can not proceed your request.']], 500);
+        }
     }
 }
